4f1240a1944fd51b8c220229fa47103cb6e51e37
Pavel Kalinin <pkalinin15@gmail.com>
2022-01-21 05:35:39 +0300

    First commit

---
+++ force-app/main/default/lwc/childAuditor/childAuditor.js
@@ -0,0 +1,47 @@
+import { LightningElement, api, track} from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+import getChildsToCountMap from '@salesforce/apex/ChildAuditorController.getChildsToCountMap';
+export default class ChildAuditor extends LightningElement {
+    @api recordId;
+    
+    @track childWithCountStrings = [];
+    
+    connectedCallback(){
+        this.updateChildsWithCount();
+    }
+
+    refreshClick(event){
+        window.console.log('Refreshing');
+        this.updateChildsWithCount();
+    }
+
+    updateChildsWithCount(){
+        window.console.log('Updating');
+        this.childWithCountStrings = [];
+        getChildsToCountMap({
+            recordId: this.recordId
+        })
+            .then(childToCountMap => {
+                refreshApex(childToCountMap);
+                window.console.log('Apex worked');
+                var conts = childToCountMap;
+                var num = 0;
+                for(var key in conts){
+                    var value = conts[key];
+                    if (value > 0){
+                        num++;
+                        if (value == 1){
+                            var ending = 'record';
+                        } else {
+                            var ending = 'records';
+                        }
+                        var childWithCount = num + '. ' + key + ' - ' + value + ' ' + ending;
+                        window.console.log('Adding: ' + childWithCount);
+                        this.childWithCountStrings.push(childWithCount);
+                    }
+                }
+            }).catch(error => {
+                window.console.log(error);
+            });
+    }
+}
